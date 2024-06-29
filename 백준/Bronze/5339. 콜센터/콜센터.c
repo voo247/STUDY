ea3d097205245e6bd5ff57f326faca575f0894dd
박예원 <160998997+voo247@users.noreply.github.com>
2024-06-29 23:01:22 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	printf("     /~\\\n");
+	printf("    ( oo|\n");
+	printf("    _\\=/_\n");
+	printf("   /  _  \\\n");
+	printf("  //|/.\\|\\\\\n");
+	printf(" ||  \\ /  ||\n");
+	printf("============\n");
+	printf("|          |\n");
+	printf("|          |\n");
+	printf("|          |\n");
+
+	return 0;
+}
