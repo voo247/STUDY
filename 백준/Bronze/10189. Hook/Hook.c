37cf1e9e3d6050ab5b196ac10fed8f896760c8c7
박예원 <160998997+voo247@users.noreply.github.com>
2024-06-29 23:10:08 +0900

    [Bronze V] Title: Hook, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	printf("#  # #### #### #  #\n");
+	printf("#### #  # #  # # #\n");
+	printf("#### #  # #  # # #\n");
+	printf("#  # #### #### #  #\n");
+
+	return 0;
+}
