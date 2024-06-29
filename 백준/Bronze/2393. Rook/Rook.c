56cb8ba8540833d85f350621883cd81a7b899a27
박예원 <160998997+voo247@users.noreply.github.com>
2024-06-29 23:28:07 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	printf("  ___  ___  ___\n");
+	printf("  | |__| |__| |\n");
+	printf("  |           |\n");
+	printf("   \\_________/\n");
+	printf("    \\_______/\n");
+	printf("     |     |\n");
+	printf("     |     |\n");
+	printf("     |     |\n");
+	printf("     |     |\n");
+	printf("     |_____|\n");
+	printf("  __/       \\__\n");
+	printf(" /             \\\n");
+	printf("/_______________\\\n");
+}
