a60299152046135d864c9e9692e6e605d071f643
박예원 <160998997+voo247@users.noreply.github.com>
2024-06-30 21:49:51 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int solved = 97;
+    char id[] = "20230795";
+
+	printf("%d\n", solved);
+	printf("%s\n", id);
+    
+    return 0;
+}
